cc799c2fac64255007968795f13511b5d65c975e
Totiruzi <totiruzi@yahoo.com>
2023-07-25 19:13:18 +0300

    Add Vee-Validation plugin

--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import VeeValidationPlugin from './includes/validation.js'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VeeValidationPlugin)
 
 app.mount('#app')
