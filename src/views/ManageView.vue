e377bb17eca1495fa4e76fcd6cf63e284ba07a73
Totiruzi <totiruzi@yahoo.com>
2023-07-29 15:39:24 +0300

    Finish Manage View and Composition view

--- src/views/ManageView.vue
+++ src/views/ManageView.vue
@@ -1,8 +1,9 @@
 <template>
+  <!-- Main Content -->
   <section class="container mx-auto mt-6">
     <div class="md:grid md:grid-cols-3 md:gap-4">
       <div class="col-span-1">
-        <app-upload ref="upload" />
+        <app-upload ref="upload" :addSong="addSong" />
       </div>
       <div class="col-span-2">
         <div class="bg-white rounded border border-gray-200 relative flex flex-col">
@@ -12,17 +13,28 @@
           </div>
           <div class="p-6">
             <!-- Composition Items -->
-            <app-composition-item v-for="(song, i) in songs" :key="song.docID" :song="song" />
+            <app-composition-item
+              v-for="(song, i) in songs"
+              :key="song.docID"
+              :song="song"
+              :updateSong="updateSong"
+              :index="i"
+              :removeSong="removeSong"
+              :updateUnsavedFlag="updateUnsavedFlag"
+            />
           </div>
         </div>
       </div>
     </div>
   </section>
 </template>
+
 <script>
+// import useUserStore from "@/stores/user";
 import AppUpload from '@/components/AppUpload.vue'
 import { songsCollection, auth } from '@/includes/firebase'
 import AppCompositionItem from '@/components/AppCompositionItem.vue'
+
 export default {
   name: 'Manage',
   components: {
@@ -31,24 +43,56 @@ export default {
   },
   data() {
     return {
-      songs: []
+      songs: [],
+      unsavedFlag: false
     }
   },
-
   async created() {
-    const snapshot = await songsCollection.where('uid', '==', 'auth.currentUser.uid').get()
-    snapshot.forEach((document) => {
+    const snapshot = await songsCollection.where('uid', '==', auth.currentUser.uid).get()
+
+    snapshot.forEach(this.addSong)
+  },
+  methods: {
+    updateSong(i, values) {
+      this.songs[i].modified_name = values.modified_name
+      this.songs[i].genre = values.genre
+    },
+    removeSong(i) {
+      this.songs.splice(i, 1)
+    },
+    addSong(document) {
       const song = {
         ...document.data(),
         docID: document.id
       }
+
       this.songs.push(song)
-    })
+    },
+    updateUnsavedFlag(value) {
+      this.unsavedFlag = value
+    }
+  },
+  beforeRouteLeave(to, from, next) {
+    if (!this.unsavedFlag) {
+      next()
+    } else {
+      // eslint-disable-next-line no-alert, no-restricted-globals
+      const leave = confirm('You have unsaved changes. Are you sure you want to leave?')
+      next(leave)
+    }
   }
-  // An alternative of getting access to child prperties (PaymentMethodChangeEvent, variable ....)
   // beforeRouteLeave(to, from, next) {
   //   this.$refs.upload.cancelUploads();
   //   next();
-  // }
+  // },
+  // beforeRouteEnter(to, from, next) {
+  //   const store = useUserStore();
+
+  //   if (store.userLoggedIn) {
+  //     next();
+  //   } else {
+  //     next({ name: "home" });
+  //   }
+  // },
 }
 </script>
