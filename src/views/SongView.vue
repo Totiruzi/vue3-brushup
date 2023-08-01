9ad40e863d4f13d4bba99439c1d347698a2a3092
Totiruzi <totiruzi@yahoo.com>
2023-08-01 11:33:45 +0300

    Finish viewing a selected song and updating comments

--- src/views/SongView.vue
+++ src/views/SongView.vue
@@ -25,7 +25,7 @@
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
         <!-- Comment Count -->
-        <span class="card-title">Comments (15)</span>
+        <span class="card-title">Comments {{ song.comment_count }}</span>
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
@@ -121,6 +121,10 @@ export default {
       this.$router.push({ name: 'home' })
       return
     }
+
+    const { sort } = this.$route.query
+    this.sort = sort === '1' || sort === '2' ? sort : '1'
+
     this.song = docSnapshot.data()
 
     this.getComments()
@@ -141,6 +145,12 @@ export default {
       }
 
       await commentsCollection.add(comment)
+
+      this.song.comment_count += 1
+      // update tje comment count in the database
+      await songsCollection.doc(this.$route.params.id).update({
+        comment_count: this.song.comment_count
+      })
 
       this.getComments()
 
@@ -163,6 +173,17 @@ export default {
           ...doc.data()
         })
       ])
+    }
+  },
+  watch: {
+    sort(newValue) {
+      //If queryParams allready matches sort value
+      if (newValue === this.$route.query.sort) return
+      this.$router.push({
+        query: {
+          sort: newValue
+        }
+      })
     }
   }
 }
